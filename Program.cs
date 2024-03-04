0b8d10e1e5fe4d7de8f8773eaff938844218f0fd
José Mauricio Suzuki Portella <craftgutin@gmail.com>
2024-03-04 10:29:02 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Menu();
+    }
+
+    static void Menu()
+    {
+
+        Console.WriteLine("M -> Tempo em minutos, EX: 10M");
+        Console.WriteLine("S -> Tempo em segundos, EX: 10S");
+        Console.WriteLine("0 - Sair");
+        string opcao = Console.ReadLine().ToLower();
+        char type = char.Parse(opcao.Substring(opcao.Length - 1, 1));
+        int time = int.Parse(opcao.Substring(0, opcao.Length - 1));
+
+        int multiplicador = 1;
+
+        if (type == 'm')
+        {
+            multiplicador = 60;
+        }
+
+        Start(time * multiplicador);
+
+
+    }
+
+    static void Start(int tempo)
+    {
+        int tempoAtual = 0;
+
+        while (tempoAtual != tempo)
+        {
+            Console.Clear();
+            tempoAtual++;
+            Console.WriteLine(tempoAtual);
+
+            Thread.Sleep(1000);
+
+        }
+    }
+}
